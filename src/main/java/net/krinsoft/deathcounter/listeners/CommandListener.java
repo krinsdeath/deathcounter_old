45de217137068829d581b07f9948501c2163022e
Jeff Wardian <krinsdeath@hotmail.com>
2011-08-10 05:30:56 -0700

    Formatted spaces to tabs; merged to ltguidepull

--- src/main/java/net/krinsoft/deathcounter/listeners/CommandListener.java
+++ src/main/java/net/krinsoft/deathcounter/listeners/CommandListener.java
@@ -11,72 +11,73 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class CommandListener implements CommandExecutor {
+
 	public DeathCounter plugin;
 	public DeathLogger log;
-	
+
 	public CommandListener(DeathCounter instance) {
 		plugin = instance;
 		log = plugin.log;
 	}
-	
+
+	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (!sender.hasPermission("deathcounter.users")) {
 			// Player has no permissions
 			sender.sendMessage(ChatColor.RED + "You do not have permission to do that.");
 			return true;
 		}
-		
+
 		if (sender instanceof Player) {
 			if (plugin.players.get((Player) sender) == null) {
 				Player p = (Player) sender;
 				plugin.players.put(p, new DeathPlayer(plugin, p.getName()));
 			}
 		}
-		
+
 		// Set up the maximum loops, and make sure the player doesn't specify something too high
 		int loops = Math.min(args.length >= 2 && args[1].matches("[1-9](?:[0-9]+)?") ? Integer.parseInt(args[1]) : plugin.config.getInt("settings.leaderboards", 5), 10);
-		if (loops < 1) loops = 1;
-		
+		if (loops < 1) {
+			loops = 1;
+		}
+
 		// default request if no args or 1st arg is 'leaders'
 		String field = "total";
-		
+
 		if (args.length > 0 && !args[0].equalsIgnoreCase("leaders")) {
 			if (args[0].equalsIgnoreCase("reset")) {
 				if (args.length < 2) {
 					// Invalid number of arguments
 					sender.sendMessage(ChatColor.RED + "Invalid number of arguments.");
-				}
-				else if (!sender.hasPermission("deathcounter.admins")) {
+				} else if (!sender.hasPermission("deathcounter.admins")) {
 					// Player doesn't have permission for 'reset'
 					sender.sendMessage(ChatColor.RED + "You do not have permission to do that.");
-				}
-				else {
+				} else {
 					Player player = plugin.getServer().getPlayer(args[1]);
 					if (player == null || !player.getName().equalsIgnoreCase(args[1])) {
 						// unable to find exact player name online	
 						sender.sendMessage(ChatColor.RED + "Player " + args[1] + " was not found. You must enter an online player's name exactly.");
-					}
-					else {
+					} else {
 						plugin.leaders.delete(player.getName());
 						sender.sendMessage(ChatColor.GREEN + "Player " + player.getName() + "'s counter was reset.");
 					}
 				}
-				
+
 				return true;
 			}
-			
+
 			if (!args[0].matches("@.+") && !plugin.monsters.contains(args[0].toLowerCase())) {
 				// The specified argument had no handler
 				sender.sendMessage(ChatColor.RED + "An option for " + args[0] + " could not be found.");
 				return true;
 			}
-			
+
 			// got a monster!
 			field = args[0].toLowerCase();
 		}
-		
+
 		plugin.leaders.fetch(sender, field, loops);
-		
+
 		return true;
 	}
 }
--- src/main/java/net/krinsoft/deathcounter/types/Leaderboards.java
+++ src/main/java/net/krinsoft/deathcounter/types/Leaderboards.java
@@ -16,78 +16,84 @@ import org.bukkit.entity.Player;
 import org.bukkit.util.config.ConfigurationNode;
 
 public class Leaderboards {
+
 	public DeathCounter plugin;
 	public DeathLogger log;
-	
+
 	public Leaderboards(DeathCounter instance) {
 		plugin = instance;
 		log = plugin.log;
 	}
-	
+
 	public void fetch(CommandSender sender, String field, int loops) {
-		if (field.matches("@.+")) playMessage(sender, getHeaderStats(field));
-		else playMessage(sender, getHeaderRanks(field, loops));
-		
+		if (field.matches("@.+")) {
+			playMessage(sender, getHeaderStats(field));
+		} else {
+			playMessage(sender, getHeaderRanks(field, loops));
+		}
+
 		String storage = plugin.config.getString("settings.storage.type", "yaml");
-		
+
 		if (storage.equalsIgnoreCase("yaml")) {
 			fetchYaml(sender, field, loops);
-		}
-		else if (storage.equalsIgnoreCase("sqlite")) {
+		} else if (storage.equalsIgnoreCase("sqlite")) {
 			fetchSqlite(sender, field, loops);
-		}
-		else if (storage.equalsIgnoreCase("mysql")) {
+		} else if (storage.equalsIgnoreCase("mysql")) {
 			fetchMysql(sender, field, loops);
 		}
 	}
-	
+
 	private void fetchYaml(CommandSender sender, String field, int loops) {
 		if (field.equalsIgnoreCase("total") || plugin.monsters.contains(field)) {
 			LinkedList<String> names = new LinkedList<String>();
 			LinkedList<Integer> counts = new LinkedList<Integer>();
-			
+
 			int minCount = 0;
 			for (String key : plugin.users.getKeys()) {
 				int count = plugin.users.getInt(key + "." + field, 0);
-				
+
 				// has kills and (more kills than the lowest or there's room for a tie) 
 				if (count > 0 && (count > minCount || (count == minCount && counts.size() < loops))) {
 					int index;
-					for (index = 0; index < counts.size(); index++)
-						if (counts.get(index) < count) break;
-					
+					for (index = 0; index < counts.size(); index++) {
+						if (counts.get(index) < count) {
+							break;
+						}
+					}
+
 					counts.add(index, count);
 					names.add(index, key);
-					
+
 					if (counts.size() > loops) {
 						minCount = counts.removeLast();
 						names.removeLast();
 					}
 				}
 			}
-			
-			for (int i = 0; i < counts.size(); i++)
+
+			for (int i = 0; i < counts.size(); i++) {
 				playMessage(sender, getMessageRanks(sender, i + 1, names.get(i), field, counts.get(i)));
-		}
-		else {
+			}
+		} else {
 			String name = field.substring(1);
-			
+
 			ConfigurationNode node = plugin.users.getNode(name);
 			if (node == null) {
 				// no user found
 				sender.sendMessage("no stats found");
 				return;
 			}
-			
+
 			String msg = null;
-			
-			for (Map.Entry<String, Object> entry : node.getAll().entrySet())
+
+			for (Map.Entry<String, Object> entry : node.getAll().entrySet()) {
 				msg = buildMessageStats(msg, entry.getKey(), entry.getValue().toString());
-			
+			}
+
 			playMessage(sender, msg);
 		}
 	}
-	
+
 	private void fetchSqlite(CommandSender sender, String field, int loops) {
 		try {
 			Class.forName("org.sqlite.JDBC");
@@ -95,116 +101,126 @@ public class Leaderboards {
 			Statement state = conn.createStatement();
 			ResultSet rs;
 			String column = "";
-			
+
 			if (field.equalsIgnoreCase("total") || plugin.monsters.contains(field)) {
 				if (field.equalsIgnoreCase("total")) {
-					for (String mob : plugin.monsters)
+					for (String mob : plugin.monsters) {
 						column += " + `" + mob + "`";
-					
+					}
+
 					column = "(" + column.substring(3) + ")";
+				} else {
+					column = "`" + field + "`";
 				}
-				else column = "`" + field + "`";
-				
+
 				rs = state.executeQuery("SELECT `name`, " + column + " AS '" + field + "' FROM `users` ORDER BY `" + field + "` DESC LIMIT " + loops + ";");
-				
-				while (rs.next())
+
+				while (rs.next()) {
 					playMessage(sender, getMessageRanks(sender, rs.getRow(), rs.getString("name"), field, rs.getInt(field)));
-			}
-			else {
-				for (String mob : plugin.monsters)
+				}
+			} else {
+				for (String mob : plugin.monsters) {
 					column += ", `" + mob + "`";
+				}
 				rs = state.executeQuery("SELECT `name`" + column + " FROM `users` WHERE `name` = '" + field.substring(1) + "';");
-				
+
 				String msg = null;
-				if (rs.next()) for (String mob : plugin.monsters)
-					msg = buildMessageStats(msg, mob, rs.getString(mob));
-				
+				if (rs.next()) {
+					for (String mob : plugin.monsters) {
+						msg = buildMessageStats(msg, mob, rs.getString(mob));
+					}
+				}
+
 				playMessage(sender, msg);
 			}
-			
+
 			state.close();
 			conn.close();
-		}
-		catch (ClassNotFoundException e) {
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
-		}
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void fetchMysql(CommandSender sender, String field, int loops) {
 		sender.sendMessage("");
 	}
-	
+
 	private void playMessage(CommandSender sender, String msg) {
 		String[] messages = msg.replaceAll("(?i)&([0-F])", "\u00A7$1").split("<newline>");
-		for (String message : messages)
+		for (String message : messages) {
 			sender.sendMessage(message);
+		}
 	}
-	
+
 	// build normal ranks message
 	private String getMessageRanks(CommandSender sender, int rank, String name, String field, int count) {
 		String playerName = sender instanceof Player ? ((Player) sender).getName() : "";
-		
+
 		String msg;
-		if (playerName.equalsIgnoreCase(name)) msg = plugin.config.getString("messages.own_rank", "---#<rank> - <name> (<kills>)---");
-		else {
+		if (playerName.equalsIgnoreCase(name)) {
+			msg = plugin.config.getString("messages.own_rank", "---#<rank> - <name> (<kills>)---");
+		} else {
 			String ordinal = "other";
-			if (rank == 1) ordinal = "first";
-			else if (rank == 2) ordinal = "second";
-			else if (rank == 3) ordinal = "third";
-			
+			if (rank == 1) {
+				ordinal = "first";
+			} else if (rank == 2) {
+				ordinal = "second";
+			} else if (rank == 3) {
+				ordinal = "third";
+			}
+
 			msg = plugin.config.getString("messages." + ordinal + "_rank", "#<rank> - <name> (<kills>)");
 		}
-		
+
 		msg = msg.replaceAll("<rank>", Integer.toString(rank));
 		msg = msg.replaceAll("<name>", name);
 		msg = msg.replaceAll("<field>", field);
 		msg = msg.replaceAll("<kills>", Integer.toString(count));
 		return msg;
 	}
-	
+
 	// build stats message
 	private String buildMessageStats(String msg, String key, String kills) {
-		if (msg == null) msg = plugin.config.getString("messages.stats", "animals -- pig:<pig> squid:<squid> chicken:<chicken> cow:<cow> sheep:<sheep> slime:<slime> wolf:<wolf><newline>monsters -- skeleton:<skeleton> creeper:<creeper> zombie:<zombie> spider:<spider> pigzombie:<pigzombie> ghast:<ghast> player:<player><newline>total kills -- <total>");
-		
+		if (msg == null) {
+			msg = plugin.config.getString("messages.stats", "animals -- pig:<pig> squid:<squid> chicken:<chicken> cow:<cow> sheep:<sheep> slime:<slime> wolf:<wolf><newline>monsters -- skeleton:<skeleton> creeper:<creeper> zombie:<zombie> spider:<spider> pigzombie:<pigzombie> ghast:<ghast> player:<player><newline>total kills -- <total>");
+		}
+
 		return msg.replaceAll("<" + key + ">", kills);
 	}
-	
+
 	// ranks header message
 	private String getHeaderRanks(String field, int loops) {
 		String msg = plugin.config.getString("messages.header", "Leaders -Top <num>- (<field>)");
 		msg = msg.replaceAll("<field>", field);
 		return msg.replaceAll("<num>", Integer.toString(loops));
 	}
-	
+
 	// stats header message
 	private String getHeaderStats(String name) {
 		String msg = plugin.config.getString("messages.header_stats", "Stats - <name>");
 		return msg.replaceAll("<name>", name);
 	}
-	
+
 	public void delete(String name) {
 		String storage = plugin.config.getString("settings.storage.type", "yaml");
-		
+
 		if (storage.equalsIgnoreCase("yaml")) {
 			deleteYaml(name);
-		}
-		else if (storage.equalsIgnoreCase("sqlite")) {
+		} else if (storage.equalsIgnoreCase("sqlite")) {
 			deleteSqlite(name);
-		}
-		else if (storage.equalsIgnoreCase("mysql")) {
+		} else if (storage.equalsIgnoreCase("mysql")) {
 			deleteMysql(name);
 		}
 	}
-	
+
 	private void deleteYaml(String name) {
 		if (plugin.users.getKeys(name) != null) {
 			plugin.users.removeProperty(name);
 		}
 	}
-	
+
 	private void deleteSqlite(String name) {
 		try {
 			String query1 = "SELECT * FROM `users` WHERE `name` = '" + name + "';";
@@ -221,18 +237,14 @@ public class Leaderboards {
 			}
 			state.close();
 			conn.close();
-		}
-		catch (ClassNotFoundException e) {
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
-		}
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void deleteMysql(String string) {
 		// TODO Auto-generated method stub
-		
 	}
-	
 }
